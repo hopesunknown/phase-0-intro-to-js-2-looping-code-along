8a3144ce03efe1b7d2d6f201ae015a5b4fc7507c
Jennifer Tchai <jentchai@gmail.com>
2021-12-05 23:04:23 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+function writeCards(name, event) {
+    let messageArray = [];
+    for (let i = 0; i < name.length; i++) {
+        let message = `Thank you, ${name[i]}, for the wonderful ${event} gift!`
+        messageArray.push(message);
+}
+ return messageArray;
+}
+
+
+function countDown() {
+    let number = 10;
+    while (number >=0) {
+        console.log(number--);
+    }
+    return countDown;
+}
